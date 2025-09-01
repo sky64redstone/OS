b08b430e74b54fb658c28d12975ffc18cbb7b98e
sky64redstone <sky64redstone@gmail.com>
2025-09-01 11:57:31 +0200

    Added version.h file

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,5 @@
 #include "drivers/vga/text.h"
-
-#define str_welcome \
-"   ___  ____   :\n" \
-"  / _ \\/ ___|  :\n" \
-" | | | \\___ \\  :\n" \
-" | |_| |___) | :\n" \
-"  \\___/|____/  : Self-written operating system in c and asm\n" \
-"...............: version 0.0.0\n"
+#include "version.h"
 
 void kmain() {
   clear_screen();
---
+++ kernel/version.h
@@ -0,0 +1,7 @@
+#define str_welcome \
+"   ___  ____   :\n" \
+"  / _ \\/ ___|  :\n" \
+" | | | \\___ \\  :\n" \
+" | |_| |___) | :\n" \
+"  \\___/|____/  : Self-written operating system in c and asm\n" \
+"...............: version 0.0.0\n"
